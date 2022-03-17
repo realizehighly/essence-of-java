df1fd8d34e1801df70f5d05e20f1d90aadb2beae
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-17 19:43:25 +0900

    반복문

---
+++ src/control/ForState.java
@@ -0,0 +1,11 @@
+package control;
+
+public class ForState {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+
+        for(int tmp : arr){
+            System.out.println(tmp);
+        }
+    }
+}
