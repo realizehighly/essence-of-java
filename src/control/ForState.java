f98744905dcfb520c084438b849cf8cf2db610c4
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-17 21:17:28 +0900

    반복문

--- src/control/ForState.java
+++ src/control/ForState.java
@@ -7,5 +7,15 @@ public class ForState {
         for(int tmp : arr){
             System.out.println(tmp);
         }
+
+        Loop1 : for(int i=2; i<=9; i++){ //1
+            for(int j=1; j<=9; j++){ //2
+                if(j==5)
+                    break Loop1; // 1번 2번 반복문 모두 벗어남
+//                  break; // 2번 반복문만 벗어남
+                System.out.println(i+"*"+j+"="+ i*j);
+            }
+            System.out.println();
+        }
     }
 }
