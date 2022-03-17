d96b3a463b1303888fc4d9beb0562adac0ba6097
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-17 18:21:56 +0900

    조건문

---
+++ src/ifstatement/IfState.java
@@ -0,0 +1,38 @@
+package ifstatement;
+
+public class IfState {
+    public static void main(String[] args) {
+
+        int x;
+        if((x=3)>0)
+            System.out.println(x);
+        String mode = "a";
+
+
+//        if(조건식1){
+//            // 조건식1 이 참(true)일 때 수행
+//            if(조건식2){
+//                // 조건식1과 조건식2가 모두 true일 때 수행
+//            } else {
+//                // 조건식1이 true이고, 조건식2가 false일 때 수행
+//            }
+//        } else {
+//            // 조건식1이 false일 때 수행
+//        }
+
+//        switch(조건식){
+//            case 값1 :
+//                // 조건식의 결과가 값1과 같은 경우 수행
+//                // ...
+//                break;
+//            case 값2 :
+//                // 조건식의 결과가 값2와 같은 경우 수행
+//                // ...
+//                break; // switch문을 벗어난다.
+//            // ...
+//            default:
+//                //조건식의 결과와 일치하는 case문이 없을 때 수행
+//                // ...
+//        }
+    }
+}
