9dd91eb50ff660d10e9289da2d6b37390a34bfe7
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-22 21:30:20 +0900

    변수와 메서드

---
+++ src/oop/ReferenceParamEx.java
@@ -0,0 +1,20 @@
+package oop;
+
+
+
+public class ReferenceParamEx {
+    public static void main(String[] args) {
+
+        int[] x = {10};
+        System.out.println("main() : x = " + x[0]);
+
+        change(x);
+        System.out.println("After change(d)");
+        System.out.println("main() : x = " + x[0]);
+    }
+
+    static void change(int[] x) {
+        x[0] = 1000;
+        System.out.println("change() : x = " + x[0]);
+    }
+}
