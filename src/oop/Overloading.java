5a9f59d014a26dcfc79cbf46e426bdcbda6597e2
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-23 20:55:15 +0900

    오버로딩

---
+++ src/oop/Overloading.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class Overloading {
+//    //매개변수의 이름만 다르기 때문에 오버로딩이 성립하지 않는다.
+//    int add(int a, int b){}
+//    int add(int x, int y){}
+//
+//    //리턴타입만 다르기 때문에 오버로딩이 성립하지 않는다.
+//    int add(int a, int b){}
+//    long add(int a, int b){}
+
+    public static void main(String[] args) {
+
+    }
+}
