c25c3cb46671292d3a64cae4dd594c9baf5847f0
Heonjun Jeong <gjswns1991@gmail.com>
2022-03-24 17:21:32 +0900

    변수의 초기화

---
+++ src/oop/InitTest.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class InitTest {
+    int x;     // 인스턴스 변수
+    int y = x; // 인스턴스 변수
+
+
+    void method1(){
+        int i;     // 지역변수
+        //int j = i; //에러. 지역변수를 초기화하지 않고 사용
+    }
+}
